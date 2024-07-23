3d4dbd85964d9cb0d5cfdbedac65147b54df908b
Daniel1O24 <166189541+Daniel1O24@users.noreply.github.com>
2024-07-23 21:14:32 +0700

    Update OTX.py
    add Indicator Facts

--- OTX.py
+++ OTX.py
@@ -3,25 +3,24 @@ import pandas as pd
 from datetime import datetime
 
 class OTX(object):
-
-    """
-    Get the IPv4 addresses list from the csv file: IPv4.csv
-    """
     def get_ipv4_list(self):
+        """
+        Get the IPv4 addresses list from the csv file: IPv4.csv
+        """
         df = pd.read_csv('IPv4.csv')
         ip_list = df['IPv4'].tolist()
         print(ip_list)
         return ip_list
 
-    """
-    Get the threat intelligence of one IPv4 address from OTX
-    """
     def get_ipv4_otx(self, ip: str):
+        """
+        Get the threat intelligence of one IPv4 address from OTX
+        """
         ipv4_otx = []
         url = "https://otx.alienvault.com/api/v1/indicators/IPv4/" + str(ip)
 
         retry = 0
-        # resilience design: if some exception happens, try three times.
+        # resilience design: if some exception happens, try three times then break the while loop
         while True:
             try:
                 response = requests.get(url)
@@ -88,13 +87,22 @@ class OTX(object):
                         asn = 'Exception:' + str(e)
                         print(asn)
 
-                    ipv4_otx = [ip, pulse_count, false_positive_str, validation_str, location, asn, str(tags_size) + " Related Tags: \n" + tags_str]
+                    try:
+                        indicator_facts_list = self.get_ip_otx_analysis(ip)
+                        indicator_facts_str = ''
+                        for indicator_fact in indicator_facts_list:
+                            indicator_facts_str = indicator_facts_str + indicator_fact + "\n"
+                    except Exception as e:
+                        indicator_facts_str = 'Exception:' + str(e)
+                        print(indicator_facts_str)
+
+                    ipv4_otx = [ip, pulse_count, false_positive_str, validation_str, location, asn, indicator_facts_str, str(tags_size) + " Related Tags: \n" + tags_str]
                 else:
                     retry += 1
                     if retry < 3:
                         continue
                     else:
-                        ipv4_otx = [ip, "status_code:" + str(status_code), "status_code:" + str(status_code), "status_code:" + str(status_code), "status_code:" + str(status_code), "status_code:" + str(status_code), "status_code:" + str(status_code)]
+                        ipv4_otx = [ip, "status_code:" + str(status_code), "status_code:" + str(status_code), "status_code:" + str(status_code), "status_code:" + str(status_code), "status_code:" + str(status_code), "status_code:" + str(status_code), "status_code:" + str(status_code)]
                 print(ipv4_otx)
                 break
             except Exception as e:
@@ -107,10 +115,10 @@ class OTX(object):
                     break
         return ipv4_otx
 
-    """
-    Get the threat intelligence of IPv4 addresses in a list from OTX
-    """
     def get_ipv4_list_otx(self, ip_list: list):
+        """
+        Get the threat intelligence of IPv4 addresses in a list from OTX
+        """
         otx_list = []
         size = len(ip_list)
         i = 0
@@ -121,14 +129,31 @@ class OTX(object):
             otx_list.append(ip_otx)
         return otx_list
 
-    """
-    Export the threat intelligence of IPv4 addresses from OTX to a .csv file 
-    """
     def export_ipv4_otx_csv(self, ip_otx_list: list):
-        df = pd.DataFrame(ip_otx_list, columns=list(['IP', 'Pulse', 'False Positive', 'Validation', 'Location', 'ASN', 'Related Tags']))
+        """
+        Export the threat intelligence of IPv4 addresses from OTX to a .csv file
+        """
+        df = pd.DataFrame(ip_otx_list, columns=list(['IP', 'Pulse', 'False Positive', 'Validation', 'Location', 'ASN',  'Indicator Facts', 'Related Tags']))
         file_time = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
         df.to_csv("IPv4_OTX_" + str(file_time) + ".csv", index=False)
 
+    def get_ip_otx_analysis(self, ip: str):
+        """
+        Get the threat intelligence anaylysis of the IP from OTX
+        :param ip:  the IP need to be analyzed by OTX
+        :return:
+        """
+        url = "https://otx.alienvault.com/otxapi/indicators/ip/analysis/" + ip
+        analysis = requests.get(url).json()
+        facts = analysis['facts']
+        keys = facts.keys()
+        indicator_facts = []
+        for key in keys:
+            fact_status = facts[key]
+            if fact_status is True:
+                key = str(key).replace("_"," ")
+                indicator_facts.append(key)
+        return indicator_facts
 
 if __name__ == '__main__':
     otx = OTX()
